6e21d494760325e8f14ebd6f2a5ddb783bbdb18f
kof.kofktu <kof.kofktu@kakaocorp.com>
2016-07-29 16:20:36 +0900

    create Framework Project

---
+++ KUIButtonBar/Classes/KUIButtonBar.swift
@@ -0,0 +1,15 @@
+//
+//  KUIButtonBar.swift
+//  KUIButtonBar
+//
+//  Created by kofktu on 2016. 7. 29..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+import UIKit
+
+public class KUIButtonBar: UIView {
+    
+    
+
+}
---
+++ KUIButtonBar/KUIButtonBar.h
@@ -0,0 +1,19 @@
+//
+//  KUIButtonBar.h
+//  KUIButtonBar
+//
+//  Created by kofktu on 2016. 7. 29..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIButtonBar.
+FOUNDATION_EXPORT double KUIButtonBarVersionNumber;
+
+//! Project version string for KUIButtonBar.
+FOUNDATION_EXPORT const unsigned char KUIButtonBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIButtonBar/PublicHeader.h>
+
+
